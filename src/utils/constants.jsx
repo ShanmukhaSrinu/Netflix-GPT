df513fcc1c02e5bd1789812b728b990308eec872
Shanmukha Srinu <ss0261676@gmail.com>
2026-01-22 23:56:37 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
   const TMDB_API_KEY=import.meta.env.VITE_TMDB_API_KEY
- export const GROG_API_KEY = import.meta.env.VITE_GROQ_API_KEY;
+
  
  export const NETFLIX_URL="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-01-09/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
