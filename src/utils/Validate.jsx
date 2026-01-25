6030a95d1c50fcdbb204d00e9272778dbc574361
shanmukha srinu <ss0261676@email.com>
2026-01-25 13:42:22 +0530

    updated password validation

--- src/utils/Validate.jsx
+++ src/utils/Validate.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const checkvalidData = (email,password) => {
  
     const isEmailValid=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-    const isPasswordValid=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+   const isPasswordValid = password && password.length > 0
 
     if(!isEmailValid){
         return "Email is not valid"
